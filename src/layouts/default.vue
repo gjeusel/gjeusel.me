d68c6b9e830edd8979a6f4220e6598fb9d8a9d75
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2022-03-20 19:08:13 +0100

    [IMP] remove debug layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <main class="px-4 text-gray-700 dark:text-gray-200">
     <router-view />
-    <div class="absolute opacity-25 text-sm bottom-0 right-0">
-      [Default Layout]
-    </div>
+    <!-- <div class="absolute opacity-25 text-sm bottom-0 right-0"> -->
+    <!--   [Default Layout] -->
+    <!-- </div> -->
   </main>
 </template>
--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="w-full px-6 md:w-2/3 md:px-0 mx-auto">
     <router-view />
-    <div class="absolute opacity-25 text-sm bottom-0 right-0">
-      [Home Layout]
-    </div>
+    <!-- <div class="absolute opacity-25 text-sm bottom-0 right-0"> -->
+    <!--   [Home Layout] -->
+    <!-- </div> -->
   </div>
   <Footer />
 </template>
